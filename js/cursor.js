b7753d0ff1bd5753760d380507b90eb96e8f0a9d
Twisterday <garayda.rostik@gmail.com>
2023-02-23 22:01:11 -0700

    add js cursor for webpage

---
+++ js/cursor.js
@@ -0,0 +1,17 @@
+let cursor = document.querySelector('.cursor');
+let a = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function (e) {
+	let x = e.clientX;
+	let y = e.clientY;
+	cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
+});
+
+a.forEach(item => {
+	item.addEventListener('mouseover', () => {
+		cursor.classList.add('hover');
+	});
+	item.addEventListener('mouseleave', () => {
+		cursor.classList.remove('hover');
+	});
+})
