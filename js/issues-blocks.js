0ad7dda099ea3ea067944bb1440f2a79bfbdf3f6
SerhiiBodnar <119057153+SerhiiBodnar@users.noreply.github.com>
2023-02-26 19:40:07 +0000

    folder api and file blocks added

--- js/issues-blocks.js
+++ js/issues-blocks.js
@@ -1,28 +1,7 @@
-(function () {
+(async function () {
 
-    const blocks = [
-        {
-            id: 1,
-            character: '1,5 °C',
-            info: 'is the increase of the average temperature around the world',
-            time: 'in 30 years'
-        }, {
-            id: 2,
-            character: '4',
-            info: 'species of animals became extinct because of humans activity.',
-            time: 'In XIX-XX centuries'
-        }, {
-            id: 3,
-            character: '9 CM',
-            info: 'is the rise of the global average sea level resulted in the melting ice.',
-            time: 'Between 1901-2010'
-        }, {
-            id: 4,
-            character: '60 000',
-            info: 'of human deaths were caused by the natural disasters.',
-            time: 'Per year, since 1960s.'
-        }
-    ];
+    const responce = await fetch('api/blocks.json');
+    const blocks = await responce.json();
 
     function renderBlocks(blocks) {
         const blocksContainer = document.querySelector('.issues__blocks');
