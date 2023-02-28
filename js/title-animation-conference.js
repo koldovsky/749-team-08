4948ca4912e9445a34bad69f83581ab452ce66e9
SofiAKOS <123480309+SofiAKOS@users.noreply.github.com>
2023-02-28 19:21:35 +0000

    Update animation

--- js/title-animation-conference.js
+++ js/title-animation-conference.js
@@ -1,14 +1,29 @@
-var textWrapper = document.querySelector('.conference__title');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+let textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
 anime.timeline({loop: true})
   .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
   }).add({
-    targets: '.ml3',
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
