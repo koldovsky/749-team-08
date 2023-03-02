1b5128792b4c73a5d699206dd32936f2d0634082
Панчишин Юлія <yhumenna@gng.com.ua>
2023-03-02 16:40:15 +0200

    fix animation in section content

--- js/content-title-animation.js
+++ js/content-title-animation.js
@@ -1,15 +1,15 @@
 (function () {
-    var textWrapper = document.querySelector('.title__animation .title__letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='title__letter'>$&</span>");
+    var textWrapper = document.querySelector('.content-title__animation .content-title__letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='content-title__letter'>$&</span>");
     anime.timeline({ loop: true })
         .add({
-            targets: '.title__animation .title__letter',
+            targets: '.content-title__animation .content-title__letter',
             scale: [0, 1],
             duration: 1500,
             elasticity: 600,
             delay: (el, i) => 45 * (i + 1)
         }).add({
-            targets: '.title__animation',
+            targets: '.content-title__animation',
             opacity: 0,
             duration: 1000,
             easing: "easeOutExpo",
