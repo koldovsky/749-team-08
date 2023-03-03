c342f1ac69d7f4faf2592a946e2c9708dd2541b0
yuliia.horenchuk <123793493+yuliia1003@users.noreply.github.com>
2023-03-03 01:02:28 +0000

    add js for section schedule

--- js/schedule.js
+++ js/schedule.js
@@ -19,5 +19,18 @@
         heading.style.transform = `scale(${scale})`;
     }, 50);
 
-    
+
+    const cloud = document.getElementById('cloud');
+    let x = -150;
+
+    function moveCloud() {
+        x += 1;
+        cloud.style.left = x + 'px';
+
+        if (x > window.innerWidth) {
+            x = -150;
+        }
+    }
+
+    setInterval(moveCloud, 10);
 })();
