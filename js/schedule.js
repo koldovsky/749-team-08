f5b33d0c7fb1bdce8e9eacc59012ebf61e4478f6
yuliia.horenchuk <123793493+yuliia1003@users.noreply.github.com>
2023-03-03 00:27:48 +0000

    ad js for title in section schrdule

--- js/schedule.js
+++ js/schedule.js
@@ -1,10 +1,23 @@
 (() => {
-    const cards = document.querySelectorAll('.card');
+    const heading = document.querySelector('.schedule__title');
+    let scale = 1;
+    let isScalingUp = true;
 
-    cards.forEach(card => {
-        card.addEventListener('click', function () {
-            card.classList.toggle('is-flipped');
-        });
-    });
+    setInterval(() => {
+        if (scale >= 1) {
+            isScalingUp = false;
+        } else if (scale <= 0.9) {
+            isScalingUp = true;
+        }
 
+        if (isScalingUp) {
+            scale += 0.01;
+        } else {
+            scale -= 0.01;
+        }
+
+        heading.style.transform = `scale(${scale})`;
+    }, 50);
+
+    
 })();
